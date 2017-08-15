90604d123c07cf6be2be4b4b580aab1f648c91f4
Eric Abell <ericabell@users.noreply.github.com>
2017-08-15 14:36:00 -0500

    hello world server up

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 const express = require('express');
 const data = require('./public/data.js');
+
+let app = express();
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+})
+
+app.listen(3000, () => {
+  console.log('Listening on 3000.');
+})
