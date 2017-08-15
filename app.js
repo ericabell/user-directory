5ee6edfb4264812a61889c83b7651495bafce778
Eric Abell <ericabell@users.noreply.github.com>
2017-08-15 14:40:48 -0500

    basic mustache template constructed

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const mustacheExpress = require('mustache-express');
 const data = require('./public/data.js');
 
 let app = express();
 
+app.engine('mustache', mustacheExpress());
+app.set('views', './views');
+app.set('view engine', 'mustache');
+
 app.get('/', (req, res) => {
-  res.send('hello world');
+  res.render('directory', data);
 })
 
 app.listen(3000, () => {
